4b3cfe7b4b2a89b509fe6ec6ba40497a065208bc
Taylor <taylor@oakcity.io>
2022-09-09 11:51:09 -0400

    add snackbar notification to frontend when serviceworker detects new code, prompt user to refresh

--- {{cookiecutter.project_slug}}/frontend/src/App.vue
+++ {{cookiecutter.project_slug}}/frontend/src/App.vue
@@ -19,6 +19,10 @@
       </v-main>
       <router-view v-else />
       <NotificationsManager></NotificationsManager>
+      <v-snackbar v-model="updateExists" color="primary" :timeout="0">
+        New content is available; please refresh.
+        <v-btn text @click.native="refreshApp">Refresh</v-btn>
+      </v-snackbar>
     </v-app>
   </div>
 </template>
@@ -35,12 +39,39 @@ import { dispatchCheckLoggedIn } from "@/store/main/actions";
   },
 })
 export default class App extends Vue {
+  public updateExists = false;
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  public registration: any = null;
+  public refreshing = false;
+
   get loggedIn() {
     return readIsLoggedIn(this.$store);
   }
 
   public async created() {
+    // This comes from
+    // https://dev.to/drbragg/handling-service-worker-updates-in-your-vue-pwa-1pip
+    document.addEventListener("swUpdated", this.updateAvailable, { once: true });
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (this.refreshing) return;
+      this.refreshing = true;
+      // Here the actual reload of the page occurs
+      window.location.reload();
+    });
     await dispatchCheckLoggedIn(this.$store);
+  }
+
+  updateAvailable(event) {
+    this.registration = event.detail;
+    this.updateExists = true;
+  }
+
+  refreshApp() {
+    this.updateExists = false;
+    // Make sure we only send a 'skip waiting' message if the SW is waiting
+    if (!this.registration || !this.registration.waiting) return;
+    // Send message to SW to skip the waiting and activate the new SW
+    this.registration.waiting.postMessage({ type: "SKIP_WAITING" });
   }
 }
 </script>
